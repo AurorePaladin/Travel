8b9f939c11bf82c506ab0562b8ada75e4735f087
AurorePaladin <aurorepaladin@gmail.com>
2018-04-29 03:57:08 +0800

    change

--- Travel/src/pages/home/Home.vue
+++ Travel/src/pages/home/Home.vue
@@ -1,12 +1,22 @@
 <template>
-<div>
- <div class="home"> hello world </div>
-</div>
+  <div>
+    <home-header></home-header>
+    <home-swiper></home-swiper>
+    <home-icons></home-icons>
+  </div>
 </template>
 
 <srcipt>
+  import HomeHeader from './components/Header'
+  import HomeSwiper from './components/Swiper'
+  import HomeIcons from './components/Icons'
   export default {
     name: 'Home'
+    components: {
+      HomeHeader,
+      HomeSwiper,
+      HomeIcons
+    }
   }
 </srcipt>
 
